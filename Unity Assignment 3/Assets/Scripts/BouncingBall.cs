b515e98a708157d1762ebfcc9380f5d299866014
jjerion <56169080+jjerion@users.noreply.github.com>
2019-11-08 11:16:54 -0500

    added BouncingBall script

---
+++ Unity Assignment 3/Assets/Scripts/BouncingBall.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncingBall : MonoBehaviour
+{
+    //set speed
+    float speed = 5f;
+    //set height
+    float height = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //put object's current position in variable
+        Vector3 pos = transform.position;
+        //calculate what the new Y position will be
+        float newY = Mathf.Sin(Time.time * speed);
+        //set object Y to new Y
+        transform.position = new Vector3(pos.x, newY, pos.z) * height;
+
+        if(newY <= .4)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = new Color(100, 0, 0);
+           
+        }
+
+        if (newY >= -.4)
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = new Color(0, 100, 00);
+
+        }
+
+    }
+}
